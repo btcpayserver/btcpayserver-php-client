fd9918d3e4c0ecc5f87cf5966720a8a53b8bce54
DCentrica <hello@dcentrica.com>
2020-08-12 10:30:40 +1200

    FIX: Modified 001 & 002 tutorial scripts: - Replaced all occurances of "BitPay" with "BTCPayServer" - Used library imports instead of FQCN instantiation - Removed cruft, whitespace etc - Pulled all required config into single array

--- examples/tutorial/001_generateKeys.php
+++ examples/tutorial/001_generateKeys.php
@@ -1,23 +1,38 @@
 <?php
-// If you have not already done so, please run `composer.phar install`
-#where to store keys
-$key_dir = __DIR__ . '/tmp';
-require __DIR__ . '/../../vendor/autoload.php';
-
 /**
- * Start by creating a PrivateKey object
+ * This script needs to be run only once per BTCPayServer setup. If you need to onnect your web-store
+ * to a completely new BTCPayServer instance, this script will need to be run again.
+ *
+ * What does it do?
+ * 
+ * 1. Generates a private and public key, encrypts them and saves them to the filesystem on which it was run
+ * 2.  
+ *
+ * TODO:
+ * - Prompt the user for: key location + Encryption key
  */
-// You can generate a private key with only one line of code like so
-$privateKey = \BTCPayServer\PrivateKey::create($key_dir . '/btcpay.pri')->generate();
 
-// NOTE: This has overridden the previous $privateKey variable, although its
-//       not an issue in this case since we have not used this key for
-//       anything yet.
+use BTCPayServer\PrivateKey;
+use BTCPayServer\PublicKey;
+use BTCPayServer\Storage\EncryptedFilesystemStorage;
 
-/**
- * Once we have a private key, a public key is created from it.
- */
-$publicKey = new \BTCPayServer\PublicKey($key_dir . '/btcpay.pub');
+// Require composer-generated autoloader
+require __DIR__ . '/../../../../../vendor/autoload.php';
+
+define('PRIVATE_KEY_NAME', 'btcpay.pri');
+define('PUBLIC_KEY_NAME', 'btcpay.pub');
+
+// Config, to come from user input
+$config = [
+    'keydir' => '/tmp/',
+    'enckey' => 'YourTopSecretPassword',
+];
+
+// Start by creating a PrivateKey object
+$privateKey = PrivateKey::create($config['keydir'] . PRIVATE_KEY_NAME)->generate();
+
+// Once we have a private key, a public key is created from it.
+$publicKey = new PublicKey($config['keydir'] . PUBLIC_KEY_NAME);
 
 // Inject the private key into the public key
 $publicKey->setPrivateKey($privateKey);
@@ -25,27 +40,22 @@ $publicKey->setPrivateKey($privateKey);
 // Generate the public key
 $publicKey->generate();
 
-// NOTE: You can again do all of this with one line of code like so:
-// `$publicKey = \BTCPayServer\PublicKey::create('/tmp/bitpay.pub')->setPrivateKey($privateKey)->generate();`
-
 /**
  * Now that you have a private and public key generated, you will need to store
  * them somewhere. This optioin is up to you and how you store them is up to
  * you. Please be aware that you MUST store the private key with some type
  * of security. If the private key is compromised you will need to repeat this
  * process.
+ *
+ * It's recommended that you use the `EncryptedFilesystemStorage` engine to persist your
+ * keys. You can, of course, create your own as long as it implements the `StorageInterface`
  */
-
-/**
- * It's recommended that you use the EncryptedFilesystemStorage engine to persist your
- * keys. You can, of course, create your own as long as it implements the StorageInterface
- */
-$storageEngine = new \BTCPayServer\Storage\EncryptedFilesystemStorage('TopSecretPassword');
+$storageEngine = new EncryptedFilesystemStorage($config['enckey']);
 $storageEngine->persist($privateKey);
 $storageEngine->persist($publicKey);
 
 /**
  * This is all for the first tutorial, you can run this script from the command
- * line `php examples/tutorial/001.php` This will generate and create two files
+ * line `php vendor/btcpayserver/btcpayserer-php-client/examples/tutorial/001.php` This will generate and create two files
  * located at `/tmp/btcpay.pri` and `/tmp/btcpay.pub`
  */
--- examples/tutorial/002_pair.php
+++ examples/tutorial/002_pair.php
@@ -1,162 +1,182 @@
 <?php
 /**
- * Copyright (c) 2014-2015 BitPay
+ * This script needs to be run only once per BTCPayServer setup. If you need to onnect your web-store
+ * to a completely new BTCPayServer instance, this script will need to be run again.
  *
- * 002 - Pairing
+ * What does it do?
+ * 
+ * 1. Generates a private and public key, encrypts them and saves them to the filesystem on which it was run
+ * 2.  
  *
- * Requirements:
- *   - Basic PHP Knowledge
- *   - Private and Public keys from 001.php
- *   - Account on https://test.bitpay.com
- *   - Pairing code
+ * What you need:
+ *
+ * - The Private and Public keys generated from 001_generateKeys.php
+ * - An account with an instance of btcpayserver
+ * - A "Pairing code" generated from within yur instance of BTCPayServer
+ *
+ * TODO:
+ * - Prompt the user for: key location + Encryption key
  */
-$key_dir = '/tmp';
-require __DIR__ . '/../../vendor/autoload.php';
 
+use BTCPayServer\Storage\EncryptedFilesystemStorage;
+use BTCPayServer\Client\Client;
+use BTCPayServer\Client\Adapter\CurlAdapter;
+use BTCPayServer\SinKey;
+
+// Require composer-generated autoloader
+require __DIR__ . '/../../../../../vendor/autoload.php';
+
+define('PRIVATE_KEY_NAME', 'btcpay.pri');
+define('PUBLIC_KEY_NAME', 'btcpay.pub');
+
+// Config, to come from user input
+$config = [
+    'keydir' => '/tmp/',
+    'enckey' => 'YourTopSecretPassword',
+    'btcpay_host' => 'https://testnet.demo.btcpayserver.org:443',
+    'pairing_code' => '8nmCvLR',
+    // An integration with BTCPay server is typically the "merchant" facade.
+    'facade' => 'merchant',
+];
 
 /**
  * To load up keys that you have previously saved, you need to use the same
  * storage engine. You also need to tell it the location of the key you want
  * to load.
  */
-$storageEngine = new \BTCPayServer\Storage\EncryptedFilesystemStorage('TopSecretPassword');
-$privateKey = $storageEngine->load($key_dir . '/bitpay.pri');
-$publicKey = $storageEngine->load($key_dir . '/bitpay.pub');
+$storageEngine = new EncryptedFilesystemStorage($config['enckey']);
+$privateKey = $storageEngine->load($config['keydir'] . PRIVATE_KEY_NAME);
+$publicKey = $storageEngine->load($config['keydir'] . PUBLIC_KEY_NAME);
 
 /**
  * Create the client, there's a lot to it and there are some easier ways, I am
  * showing the long form here to show how various things are injected into the
  * client.
  */
-$client = new \BTCPayServer\Client\Client();
+$client = new Client();
 
 /**
  * The adapter is what will make the calls to BitPay and return the response
  * from BitPay. This can be updated or changed as long as it implements the
  * AdapterInterface
  */
-$adapter = new \BTCPayServer\Client\Adapter\CurlAdapter();
+$adapter = new CurlAdapter();
 
 /**
  * Now all the objects are created and we can inject them into the client
  */
 $client->setPrivateKey($privateKey);
 $client->setPublicKey($publicKey);
-$client->setUri('https://my-btcpay-server.com:443'); //specify port
+$client->setUri($config['btcpay_host']);
 $client->setAdapter($adapter);
 
 /**
- * Visit https://test.bitpay.com/api-tokens and create a new pairing code. Pairing
- * codes can only be used once and the generated code is valid for only 24 hours.
+ * Visit your BTCPayServer instance, login and within it, create a new pairing code.
+ * Pairing codes can only be used once and the generated code is valid for only 24 hours.
  */
-$pairingCode = '8nmCvLR';
+$pairingCode = $config['pairing_code'];
 
 /**
  * Currently this part is required, however future versions of the PHP SDK will
  * be refactor and this part may become obsolete.
  */
-$sin = new \BTCPayServer\SinKey('/tmp/sin.key');
+$sin = new SinKey('/tmp/sin.key');
 $sin->setPublicKey($publicKey);
 $sin->generate();
-#$sin = \BTCPayServer\SinKey::create()->setPublicKey($publicKey)->generate();
+
 error_log('$pairingCode: ' . $pairingCode);
 error_log('$sin: ' . $sin);
-/**** end ****/
 
-
-// An integration with BTCPay server is typically the "merchant" facade.
-$facade = 'merchant';
 // 'empty' will be POS
 // payroll
 // merchant
 
-if (!$facade):
+if (!$config['facade']):
     try {
-        $token = $client->createToken(array(
+        $token = $client->createToken([
                 'pairingCode' => $pairingCode,
                 'label' => 'description',
                 'id' => (string)$sin,
-            ));
+        ]);
     } catch (\Exception $e) {
         /**
          * The code will throw an exception if anything goes wrong, if you did not
          * change the $pairingCode value or if you are trying to use a pairing
          * code that has already been used, you will get an exception. It was
          * decided that it makes more sense to allow your application to handle
-         * this exception since each app is different and has different requirements.
+         * this exception itself, since each app is different and will of course
+         * have differing requirements.
          */
         echo "Exception occured: " . $e->getMessage() . PHP_EOL;
-
         echo "Pairing failed. Please check whether you're trying to pair a production pairing code on test." . PHP_EOL;
+
         $request = $client->getRequest();
         $response = $client->getResponse();
+
         /**
          * You can use the entire request/response to help figure out what went
          * wrong, but for right now, we will just var_dump them.
          */
         echo (string)$request . PHP_EOL . PHP_EOL . PHP_EOL;
         echo (string)$response . PHP_EOL . PHP_EOL;
+
         /**
          * NOTE: The `(string)` is include so that the objects are converted to a
          *       user friendly string.
          */
-
         exit(1); // We do not want to continue if something went wrong
     }
 
     /**
      * You will need to persist the token somewhere, by the time you get to this
      * point your application has implemented an ORM such as Doctrine or you have
-     * your own way to persist data. Such as using a framework or some other code
-     * base such as Drupal.
+     * your own way to persist data. Such as using a framework or some other framework
+     * such as Laravel, Silverstripe or Drupal.
      */
     $persistThisValue = $token->getToken();
     echo 'Token obtained: ' . $persistThisValue . PHP_EOL;
 endif;
 
-if ($facade == 'merchant' || $facade == 'payroll'):
+if ($config['facade'] == 'merchant' || $config['facade'] == 'payroll'):
     try {
-        $token = $client->createToken(array(
-                'facade' => $facade,
-                'label' => 'label this token',
-                'id' => (string)$sin,
-            ));
-
+        $token = $client->createToken([
+            'facade' => $config['facade'],
+            'label' => 'label this token',
+            'id' => (string)$sin,
+        ]);
 
         echo "<pre>";
         var_dump($token);
         echo "</pre>";
 
         $pairingCode = $token->GetpairingCode();
+        $url = $config['btcpay_url'] . '/api-access-request?pairingCode=' . $pairingCode;
 
-        $url = 'https://btcpayserver.com/api-access-request?pairingCode=' . $pairingCode;
-
-        echo "\n$url";
-        echo "\n";
+        echo "\n$url\n";
         echo 'Token obtained: ' . $token->getToken() . PHP_EOL;
 
     } catch (\Exception $e) {
         echo "Exception occured: " . $e->getMessage() . PHP_EOL;
-
         echo "Pairing failed. Please check whether you're trying to pair a production pairing code on test." . PHP_EOL;
+
         $request = $client->getRequest();
         $response = $client->getResponse();
+
         /**
          * You can use the entire request/response to help figure out what went
          * wrong, but for right now, we will just var_dump them.
          */
         echo (string)$request . PHP_EOL . PHP_EOL . PHP_EOL;
         echo (string)$response . PHP_EOL . PHP_EOL;
+        
         /**
          * NOTE: The `(string)` is include so that the objects are converted to a
-         *       user friendly string.
+         * user friendly string.
          */
-
         exit(1); // We do not want to continue if something went wrong
-
     }
 endif;
 
 /**
- * Make sure you persist the token, you will need it for the next tutorial
+ * Note: Make sure you persist the token, you will need it for the next tutorial
  */
